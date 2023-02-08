cc896a969c0c9436c9b561b835418dabd229cb67
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-02-08 22:36:37 +0530

    Add: Footer Area

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import styles from './App.module.css';
 import HeaderView from "./components/HeaderView";
+import FooterView from "./components/FooterView";
 
 function App() {
 
     return (
-        <header className={styles.Header}>
-            <HeaderView />
-        </header>
+        <>
+            <header className={styles.Header}>
+                <HeaderView />
+            </header>
+            <footer className={styles.Footer}>
+                <FooterView />
+            </footer>
+        </>
     );
 }
 
---
+++ src/components/FooterView.js
@@ -0,0 +1,40 @@
+import styles from './FooterView.module.css';
+import {useContext} from "react";
+import {MobileViewContext} from "../index";
+
+function FooterView() {
+    const isMobile = useContext(MobileViewContext);
+    return (
+        <>
+            <img className={styles.FooterLogo} src={require('../assets/food-truck.png')} alt='website-logo' />
+
+            <section className={styles.ContactUs}>
+                <h1 className={styles.FooterH1}>Contact Us</h1>
+                <p>Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM Market, XYZ-343434</p>
+                <p>example2020@gmail.com</p>
+                <p>(904) 443-0343</p>
+            </section>
+
+            <section className={styles.MoreOptions}>
+                <h1 className={styles.FooterH1}>More</h1>
+                <a href='/about-us'>About Us</a>
+                <a href='/products'>Products</a>
+                <a href='/career'>Career</a>
+                <a href='/contact-us'>Contact Us</a>
+            </section>
+
+            <section className={styles.Socials}>
+                <h1 className={styles.FooterH1}>Social Links</h1>
+                <div className={styles.SocialsRow}>
+                    <a href='/products' target='_blank'><img src={require('../assets/insta.svg').default} alt='instagram-link' /></a>
+                    <a href='/products' target='_blank'><img src={require('../assets/twitter.svg').default} alt='instagram-link' /></a>
+                    <a href='/products' target='_blank'><img src={require('../assets/fb.svg').default} alt='instagram-link' /></a>
+                </div>
+
+                <p className={styles.CopyrightText}>© 2022 Food Truck Example</p>
+            </section>
+        </>
+    );
+}
+
+export default FooterView;
