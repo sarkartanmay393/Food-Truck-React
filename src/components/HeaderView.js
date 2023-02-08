45e7a26bfa4a1425d6d9a4ad061c948f6c2af3bb
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-02-08 21:05:14 +0530

    Add: First 100vh Area

---
+++ src/components/HeaderView.js
@@ -0,0 +1,27 @@
+import styles from './HeaderView.module.css';
+import {useContext} from "react";
+import {MobileViewContext} from "../index";
+
+function HeaderView() {
+    const isMobile = useContext(MobileViewContext);
+    return (
+        <>
+            <section className={styles.TopIntro}>
+                <section className={styles.CenterArea}>
+                    {isMobile ? <></> : <img src={require('../assets/food-truck.png')} alt='website-logo'/>}
+                    <h3>Discover the <br/><span>Best</span> Food and Drinks</h3>
+                    <p>Naturally made Healthcare Products for the better care & support of your body.</p>
+                    <button>Explore Now!</button>
+                </section>
+            </section>
+
+            <section className={styles.TopPizza}>
+                <img src={require('../assets/pizza.png')} alt='website-pizza'/>
+            </section>
+
+            <img className={styles.HeaderVector} src={require('../assets/vector.svg').default} alt='header-vector' />
+        </>
+    );
+}
+
+export default HeaderView;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const MobileViewContext = React.createContext();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <MobileViewContext.Provider value={window.screen.width < 600}>
+        <App />
+      </MobileViewContext.Provider>
   </React.StrictMode>
 );
 
@@ -15,3 +19,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
